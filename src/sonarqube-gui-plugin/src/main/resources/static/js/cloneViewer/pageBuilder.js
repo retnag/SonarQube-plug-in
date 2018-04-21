758ab53ef7a4be7751b23819982b3a9c35737ed1
Kutas Márton <h662452@stud.u-szeged.hu>
2018-04-21 12:22:26 +0200

    remove second cloneClassSelector, change event listener for selector menues

--- src/sonarqube-gui-plugin/src/main/resources/static/js/cloneViewer/pageBuilder.js
+++ src/sonarqube-gui-plugin/src/main/resources/static/js/cloneViewer/pageBuilder.js
@@ -7,113 +7,93 @@
 SM.pageBuilder.cloneViewer = {};
 
 SM.pageBuilder.cloneViewer.build = function() {
-    var cloneClassSelectorOptions = [
+    var cloneClassSelectorSkeleton = [
       '<select id="cloneClassSelector">',
       '</select>'
     ].join("\n");
 
-    var cloneInstanceSelectorOptions = [
+    var cloneInstanceSelectorSkeleton = [
       '<select id="cloneInstanceSelector">',
       '</select>'
     ].join("\n");
 
-    var cloneClassSelectorOptions2 = [
-      '<select id="cloneClassSelector2">',
-      '</select>'
-    ].join("\n");
-
-    var cloneInstanceSelectorOptions2 = [
+    var cloneInstanceSelector2Skeleton = [
       '<select id="cloneInstanceSelector2">',
       '</select>'
     ].join("\n");
 
-SM.getRoot().html([
-    '<div id="cloneClassSelectorContainer">',
-    '<div>CloneClassSelector: '+cloneClassSelectorOptions+'</div>',
-    '<div>CloneInstanceSelector: '+cloneInstanceSelectorOptions+'</div>',
-    '<div id="cloneViewer"></div>',
-    '</div>',
-    '<div id="cloneClassSelectorContainer2">',
-    '<div>CloneClassSelector: '+cloneClassSelectorOptions2+'</div>',
-    '<div>CloneInstanceSelector: '+cloneInstanceSelectorOptions2+'</div>',
-    '<div id="cloneViewer2"></div>',
-    '</div>'
-  ].join(""));
+    SM.getRoot().html([
+        '<div id="cloneClassSelectorContainer">',
+        '<div>CloneClassSelector: '+cloneClassSelectorSkeleton+'</div>',
+        '<div>CloneInstanceSelector: '+cloneInstanceSelectorSkeleton+'</div>',
+        '<div id="cloneViewer"></div>',
+        '</div>',
+        '<div id="cloneClassSelectorContainer2">',
+        '<div>CloneInstanceSelector: '+cloneInstanceSelector2Skeleton+'</div>',
+        '<div id="cloneViewer2"></div>',
+        '</div>'
+    ].join(""));
 
+    //
+    $( "#cloneClassSelector" )
+        .selectmenu();
 
+    $( "#cloneInstanceSelector" )
+        .selectmenu();
 
-  var temp = [];
-  SM.state[SM.options.component.key].clone.data.forEach(function(c){
-  	temp.push("<option value="+c.name+"> "+c.name+"</option>");
-  });
-  $( "#cloneClassSelector" )
-	.append(temp.join(""))
-    .selectmenu()
-    .val(SM.state[SM.options.component.key].cloneViewer.selectedCloneClassName)
-    .selectmenu("refresh");
+    $( "#cloneInstanceSelector2" )
+        .selectmenu();
 
-  var temp2 = [];
-  SM.state[SM.options.component.key].clone.data.forEach(function(c){
-  	temp2.push("<option value="+c.name+"> "+c.name+"</option>");
-  });
-  $( "#cloneClassSelector2" )
-	.append(temp2.join(""))
-    .selectmenu();
+    //eventlisteners
+    $( "#cloneClassSelector" ).on( "selectmenuselect", function( event, ui ) {
+        var cloneInstanceSelectorOptions = [];
+        SM.state[SM.options.component.key].clone.data.forEach(function(x){
+		    if(x.name === $('#cloneClassSelector').val()){
+			    x.cloneInstances.forEach(function(c){
+  				    cloneInstanceSelectorOptions.push("<option value="+c.displayedPath+"> "+c.name+"</option>");
+  			    });
+		    }
+        });
+        $( "#cloneInstanceSelector" )
+            .find('option').remove().end()
+	        .append(cloneInstanceSelectorOptions.join(""))
+            .selectmenu("destroy").selectmenu({ style: "dropdown" });
+        $( "#cloneInstanceSelector2" )
+            .find('option').remove().end()
+	        .append(cloneInstanceSelectorOptions.join(""))
+            .selectmenu("destroy").selectmenu({ style: "dropdown" });
+    });
 
-//
-//ide kéne kiváltani a cloneClassSelector selectmenuchange eseményt (76.sor)
-$( "#cloneInstanceSelector" )
-.selectmenu()
-.val(SM.state[SM.options.component.key].cloneViewer.selectedCloneInstanceName)
-.selectmenu("refresh");
-
-
-$( "#cloneInstanceSelector2" )
-  .selectmenu();
-
-
-//eventlisteners
-  $( "#cloneClassSelector" ).on( "selectmenuchange", function( event, ui ) {
-  var temp11 = [];
-  SM.state[SM.options.component.key].clone.data.forEach(function(x){
-		if(x.name === $('#cloneClassSelector').val()){
-			x.cloneInstances.forEach(function(c){
-  				temp11.push("<option value="+c.displayedPath+"> "+c.name+"</option>");
-  			});
-		}
-	});
-  $( "#cloneInstanceSelector" )
-    .find('option').remove().end()
-	  .append(temp11.join(""))
-    .selectmenu("destroy").selectmenu({ style: "dropdown" });
-  }
- );
-
-   $( "#cloneInstanceSelector" ).on( "selectmenuchange", function( event, ui ) {
+    $( "#cloneInstanceSelector" ).on( "selectmenuselect", function( event, ui ) {
    		SM.RawFileLoader.requestSliceOfRawFile(function(rawFileData){
 			$( "#cloneViewer" ).html(rawFileData.join("<br>"));
    		},$('#cloneInstanceSelector').val());
 	});
 
-$( "#cloneClassSelector2" ).on( "selectmenuchange", function( event, ui ) {
-  var temp12 = [];
-  SM.state[SM.options.component.key].clone.data.forEach(function(x){
-		if(x.name === $('#cloneClassSelector2').val()){
-			x.cloneInstances.forEach(function(c){
-  				temp12.push("<option value="+c.displayedPath+"> "+c.name+"</option>");
-  			});
-		}
-	});
-  $( "#cloneInstanceSelector2" )
-    .find('option').remove().end()
-	  .append(temp12.join(""))
-    .selectmenu("destroy").selectmenu({ style: "dropdown" });
-  }
- );
 
-   $( "#cloneInstanceSelector2" ).on( "selectmenuchange", function( event, ui ) {
+    $( "#cloneInstanceSelector2" ).on( "selectmenuselect", function( event, ui ) {
    		SM.RawFileLoader.requestSliceOfRawFile(function(rawFileData){
 			$( "#cloneViewer2" ).html(rawFileData.join("<br>"));
    		},$('#cloneInstanceSelector2').val());
 	});
+
+    //
+    var cloneClassSelectorOptions = [];
+    SM.state[SM.options.component.key].clone.data.forEach(function(c){
+  	    cloneClassSelectorOptions.push("<option value="+c.name+"> "+c.name+"</option>");
+    });
+
+    $( "#cloneClassSelector" )
+    	.append(cloneClassSelectorOptions.join(""))
+        .selectmenu()
+        .val(SM.state[SM.options.component.key].cloneViewer.selectedCloneClassName)
+        .selectmenu("refresh")
+        .trigger("selectmenuselect");
+
+    $( "#cloneInstanceSelector" )
+        .selectmenu()
+        .val(SM.state[SM.options.component.key].cloneViewer.selectedCloneInstanceName)
+        .selectmenu("refresh")
+        .trigger("selectmenuselect");
+  
 };
